2f984158fc0405eeb7674937f4ce59dee6e4dff6
Tregan Emmett <tregan@tregan.me>
2020-01-19 22:24:38 -0700

    build: add rollup

---
+++ rollup.config.ts
@@ -0,0 +1,45 @@
+import commonjs from '@rollup/plugin-commonjs';
+import external from 'rollup-plugin-peer-deps-external';
+import pkg from './package.json';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from 'rollup-plugin-typescript2';
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      exports: 'named',
+      sourcemap: true
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      exports: 'named',
+      sourcemap: true
+    }
+  ],
+  plugins: [
+    external(),
+    resolve({
+      browser: true
+    }),
+    typescript({
+      rollupCommonJSResolveHack: true,
+      clean: true
+    }),
+    commonjs({
+      include: ['node_modules/**'],
+      namedExports: {
+        'node_modules/react/react.js': [
+          'Children',
+          'Component',
+          'PropTypes',
+          'createElement'
+        ],
+        'node_modules/react-dom/index.js': ['render']
+      }
+    })
+  ]
+};
