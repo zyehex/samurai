fc06ea4bb48e0d43a87b0f46078b4484d0968313
Tregan Emmett <tregan@tregan.me>
2020-01-19 23:20:30 -0700

    feat: add prop-types

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,7 +1,10 @@
 import React, { FC } from 'react';
+import PropTypes from 'prop-types';
 
 export interface ButtonProps {
+  /** Text to display within the button */
   name: string;
+  /** Action on click */
   onClick: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 }
 
@@ -10,5 +13,10 @@ const Button: FC<ButtonProps> = ({ name, onClick }) => (
     {name}
   </button>
 );
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  name: PropTypes.string.isRequired
+};
 
 export default Button;
